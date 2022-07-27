4732404a641aa6edea354f3bbdd00a8aa399ce86
Mesut Çağa <me5ut@windowslive.com>
2022-07-27 12:07:26 +0300

    Tested Login on Payment Spec

--- README.md
+++ README.md
@@ -2,5 +2,6 @@ Thanks to team Cypress for this real world demo app ❤️
 
 <a href="https://github.com/cypress-io/cypress-realworld-app">Original repo</a>
 
-to open cypress UI
+### to open cypress UI
+
 yarn run cypress open
--- cypress/e2e/1-getting-started/payment_spec.cy.js
+++ cypress/e2e/1-getting-started/payment_spec.cy.js
@@ -1,6 +1,14 @@
 describe("payment", () => {
   it("user can make payment", () => {
     // login
+    cy.visit("localhost:3000/");
+    cy.findByRole("textbox", { name: /username/i }).type("Tavares_Barrows");
+    cy.findByLabelText(/password/i).type("s3cret");
+    cy.findByRole("checkbox", { name: /remember me/i }).check();
+    cy.findByRole("button", { name: /sign in/i }).click();
+
+    // https://www.youtube.com/watch?v=OVNjsIto9xM&t=2260s min 40:06
+
     // check balance
     // click on pay button
     // search for user
